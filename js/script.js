bcf0e06886e5bd23dd852d3ed886e28c70347d2c
Алексей Наумов <maggister78@gmail.com>
2022-03-14 16:09:21 +0300

    Function
    Signed-off-by: Алексей Наумов <maggister78@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,26 @@
+console.log('Привет');
+
+let num = 50;
+//
+// while (num < 55) {
+//     num++;
+//   console.log(num);
+// }
+// console.log('Привет');
+
+
+//
+// do {
+//   num++;
+// console.log(num);
+// } while (num < 55);
+
+
+
+for (let i = 3; i < 9; i+3) {
+  if (i===10) {
+    break;
+
+  }
+  console.log(i);
+}
