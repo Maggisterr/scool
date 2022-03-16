4a16955b6ff60d62a70fdb9a12b9c5edd3145ef7
Mag <maggisterr@rambler.ru>
2022-03-16 21:39:08 +0300

    personalMovie.count

--- js/script.js
+++ js/script.js
@@ -1,26 +1,48 @@
-console.log('Привет');
+'use strict'
 
-let num = 50;
-//
-// while (num < 55) {
-//     num++;
-//   console.log(num);
-// }
-// console.log('Привет');
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?','')
 
-
-//
-// do {
-//   num++;
-// console.log(num);
-// } while (num < 55);
-
-
-
-for (let i = 3; i < 9; i+3) {
-  if (i===10) {
-    break;
-
-  }
-  console.log(i);
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actrors: {},
+  genres: [],
+  privat: false
 }
+
+
+
+// let task1 = prompt ('Один из последних просмотренных фильмов?','');
+// let task2 = prompt ('На сколько оцените его?','');
+// let task3 = prompt ('Один из последних просмотренных фильмов?','');
+// let task4 = prompt ('На сколько оцените его?','');
+
+// personalMovieDB.movies[task1] = task2;
+// personalMovieDB.movies[task3] = task4;
+
+// console.log(personalMovieDB);
+
+for (let i = 1; i < 3; i++) {
+  let task1 = prompt ('Один из последних просмотренных фильмов?','');
+  let task2 = prompt ('На сколько оцените его?','');
+  if (task1 != null && task1 != "" && task1.length <= 50 && task2 != null && task2 != '') {
+    personalMovieDB.movies[task1] = task2;
+  } else {
+    i--
+  }
+  // console.log(personalMovieDB);
+}
+
+let a = personalMovieDB.count;
+
+if (a <= 10) {
+  console.log('Просмотренно довольно мало фильмов'); 
+} else if (10 < a && a < 30) {
+  console.log('Вы класический зритель');
+} else  if (a > 30) {
+  console.log('Вы киноман!');
+} else {
+  console.log('Произошла ошибка');
+}
+
+console.log(personalMovieDB);
