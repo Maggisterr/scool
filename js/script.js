fc7162f0b2a72135c75ff60f896cee2df6e979d0
Mag <maggisterr@rambler.ru>
2022-03-21 18:25:28 +0300

    personalMovie.count

--- js/script.js
+++ js/script.js
@@ -1,26 +1,7 @@
-console.log('Привет');
+'use strict'
 
-let num = 50;
-//
-// while (num < 55) {
-//     num++;
-//   console.log(num);
-// }
-// console.log('Привет');
+function showFirstMessage() {
+  console.log('Hello world!');
 
-
-//
-// do {
-//   num++;
-// console.log(num);
-// } while (num < 55);
-
-
-
-for (let i = 3; i < 9; i+3) {
-  if (i===10) {
-    break;
-
-  }
-  console.log(i);
 }
+showFirstMessage();
